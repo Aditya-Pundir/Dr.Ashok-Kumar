c043dfc33846558058bf2f8fe355e57b0d103ae8
Aditya Pundir <adityapundir2k@gmail.com>
2021-06-22 22:19:36 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,21 +5,27 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./css/Theme.css";
 import "./css/App.css";
 function App() {
-  let dataMap = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let dataMap = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   let data = {
-    Images: [],
-    Names: [],
+    Images: [
+      // 1
+      "https://i.pinimg.com/originals/9b/02/3f/9b023f2b842d04454b8f57cde4b5c066.jpg",
+    ],
+    Names: [
+      // 1
+      "Bouganvellia",
+    ],
     Description: [
       // 1
-      "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      "Description: Bougainvillea is a genus of thorny ornamental vines, bushes, and trees belonging to the four o' clock family, Nyctaginaceae. It is native to eastern South America, found from Brazil, west to Peru, and south to southern Argentina. Different authors accept from 4 to 18 species in the genus. The inflorescence consists of large colourful sepal-like bracts which surround three simple waxy flowers.",
       // 2
-      "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500sLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500sLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been ",
       // 3
-      "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500sLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500sLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been ",
       // 4
-      "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500sLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500sLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been ",
       // 5
-      "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500sLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500sLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been ",
       // 6
       "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
       // 7
@@ -29,6 +35,12 @@ function App() {
       // 9
       "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
       // 10
+      "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      // 11
+      "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      // 12
+      "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+      // 13
       "Description: Some random text going around - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
     ],
   };
